4f6c344d65a3ab87357e2aca7ec75776544753c3
marcusucubi <marcusucubi@e5946206-da35-2a59-e6c2-62807b22cc5c>
2012-04-21 17:32:42 +0000

    make compare user draw

--- trunk/quote/src/forms/frmCompare.Designer.vb
+++ trunk/quote/src/forms/frmCompare.Designer.vb
@@ -24,109 +24,46 @@ Partial Class frmCompare
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.ListViewSource = New System.Windows.Forms.ListView()
-        Me.ColumnHeader5 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.ColumnHeader6 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ListViewDestination = New System.Windows.Forms.ListView()
-        Me.ColumnHeader7 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
         Me.ColumnHeader8 = CType(New System.Windows.Forms.ColumnHeader(), System.Windows.Forms.ColumnHeader)
-        Me.SplitContainer1 = New System.Windows.Forms.SplitContainer()
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).BeginInit()
-        Me.SplitContainer1.Panel1.SuspendLayout()
-        Me.SplitContainer1.Panel2.SuspendLayout()
-        Me.SplitContainer1.SuspendLayout()
         Me.SuspendLayout()
-        '
-        'ListViewSource
-        '
-        Me.ListViewSource.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader5, Me.ColumnHeader6})
-        Me.ListViewSource.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.ListViewSource.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
-        Me.ListViewSource.FullRowSelect = True
-        Me.ListViewSource.GridLines = True
-        Me.ListViewSource.Location = New System.Drawing.Point(0, 0)
-        Me.ListViewSource.MultiSelect = False
-        Me.ListViewSource.Name = "ListViewSource"
-        Me.ListViewSource.Size = New System.Drawing.Size(312, 337)
-        Me.ListViewSource.TabIndex = 0
-        Me.ListViewSource.UseCompatibleStateImageBehavior = False
-        Me.ListViewSource.View = System.Windows.Forms.View.Details
-        '
-        'ColumnHeader5
-        '
-        Me.ColumnHeader5.Text = "Number"
-        Me.ColumnHeader5.Width = 1
-        '
-        'ColumnHeader6
-        '
-        Me.ColumnHeader6.Text = ""
-        Me.ColumnHeader6.Width = 1600
         '
         'ListViewDestination
         '
-        Me.ListViewDestination.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader7, Me.ColumnHeader8})
+        Me.ListViewDestination.Columns.AddRange(New System.Windows.Forms.ColumnHeader() {Me.ColumnHeader8})
         Me.ListViewDestination.Dock = System.Windows.Forms.DockStyle.Fill
         Me.ListViewDestination.Font = New System.Drawing.Font("Courier New", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.ListViewDestination.FullRowSelect = True
         Me.ListViewDestination.GridLines = True
+        Me.ListViewDestination.HeaderStyle = System.Windows.Forms.ColumnHeaderStyle.None
         Me.ListViewDestination.Location = New System.Drawing.Point(0, 0)
         Me.ListViewDestination.MultiSelect = False
         Me.ListViewDestination.Name = "ListViewDestination"
-        Me.ListViewDestination.Size = New System.Drawing.Size(356, 337)
-        Me.ListViewDestination.TabIndex = 1
+        Me.ListViewDestination.OwnerDraw = True
+        Me.ListViewDestination.Size = New System.Drawing.Size(672, 337)
+        Me.ListViewDestination.TabIndex = 3
         Me.ListViewDestination.UseCompatibleStateImageBehavior = False
         Me.ListViewDestination.View = System.Windows.Forms.View.Details
-        '
-        'ColumnHeader7
-        '
-        Me.ColumnHeader7.Text = "Number"
-        Me.ColumnHeader7.Width = 1
         '
         'ColumnHeader8
         '
         Me.ColumnHeader8.Text = ""
         Me.ColumnHeader8.Width = 1600
         '
-        'SplitContainer1
-        '
-        Me.SplitContainer1.Dock = System.Windows.Forms.DockStyle.Fill
-        Me.SplitContainer1.Location = New System.Drawing.Point(0, 0)
-        Me.SplitContainer1.Name = "SplitContainer1"
-        '
-        'SplitContainer1.Panel1
-        '
-        Me.SplitContainer1.Panel1.Controls.Add(Me.ListViewSource)
-        '
-        'SplitContainer1.Panel2
-        '
-        Me.SplitContainer1.Panel2.Controls.Add(Me.ListViewDestination)
-        Me.SplitContainer1.Size = New System.Drawing.Size(672, 337)
-        Me.SplitContainer1.SplitterDistance = 312
-        Me.SplitContainer1.TabIndex = 2
-        '
         'frmCompare
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(672, 337)
-        Me.Controls.Add(Me.SplitContainer1)
+        Me.Controls.Add(Me.ListViewDestination)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "frmCompare"
         Me.Text = "frmCompare"
-        Me.SplitContainer1.Panel1.ResumeLayout(False)
-        Me.SplitContainer1.Panel2.ResumeLayout(False)
-        CType(Me.SplitContainer1, System.ComponentModel.ISupportInitialize).EndInit()
-        Me.SplitContainer1.ResumeLayout(False)
         Me.ResumeLayout(False)
 
     End Sub
-
-    Friend WithEvents ListViewSource As System.Windows.Forms.ListView
-    Friend WithEvents ColumnHeader5 As System.Windows.Forms.ColumnHeader
-    Friend WithEvents ColumnHeader6 As System.Windows.Forms.ColumnHeader
     Friend WithEvents ListViewDestination As System.Windows.Forms.ListView
-    Friend WithEvents ColumnHeader7 As System.Windows.Forms.ColumnHeader
     Friend WithEvents ColumnHeader8 As System.Windows.Forms.ColumnHeader
-    Friend WithEvents SplitContainer1 As System.Windows.Forms.SplitContainer
+
 
 End Class
