64ad14b026f66a364ace627a1e1be2a97102b71e
marcusucubi <marcusucubi@e5946206-da35-2a59-e6c2-62807b22cc5c>
2012-06-26 15:25:54 +0000

    support for "BOM copy"

---
+++ trunk/quote/src/database/BOMCopier.vb
@@ -0,0 +1,48 @@
+﻿Imports System.Data.SqlClient
+Imports System.Transactions
+Imports DCS.Quote.Model.BOM
+Imports DCS.Quote.QuoteDataBaseTableAdapters
+
+Public Class BOMCopier
+
+    Public Function Copy(ByVal q As Header) _
+                        As Integer
+
+        frmMain.frmMain.UseWaitCursor = True
+        My.Application.DoEvents()
+
+        ' Ensure the properies are updated
+        frmMain.frmMain.Focus()
+
+        Dim o As PrimaryPropeties = q.PrimaryProperties
+
+        Dim newId As Integer
+
+        Dim adaptor As New QuoteDataBaseTableAdapters._QuoteTableAdapter
+        adaptor.Connection.Open()
+        adaptor.Transaction = adaptor.Connection.BeginTransaction
+        adaptor.Insert(o.Customer.Name, _
+            o.RequestForQuoteNumber, o.PartNumber, False, 0, _
+            o.CommonInitials, Date.Now, Date.Now, o.Customer.ID)
+        Dim cmd As SqlCommand = New SqlCommand( _
+            "SELECT @@IDENTITY", adaptor.Connection)
+        cmd.Transaction = adaptor.Transaction
+        newId = CInt(cmd.ExecuteScalar())
+        adaptor.Transaction.Commit()
+        adaptor.Connection.Close()
+
+        adaptor.Connection.Open()
+        CommonSaver.DeleteProperties(newId)
+        CommonSaver.SaveNoteProperties(newId, q.NoteProperties)
+        CommonSaver.SaveOtherProperties(newId, q.OtherProperties, True)
+        CommonSaver.SaveComputationProperties(newId, q.ComputationProperties, True)
+        CommonSaver.DeleteComponents(newId)
+        CommonSaver.SaveComponents(q, newId, True)
+        adaptor.Connection.Close()
+
+        frmMain.frmMain.UseWaitCursor = False
+
+        Return newId
+    End Function
+
+End Class
---
+++ trunk/quote/src/forms/frmCopyBOM.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Windows.Forms
+
+Public Class frmCopyBOM
+
+    Public Property Initials As String
+
+    Private Sub OK_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles OK_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.OK
+        Initials = Me.txtInitials.Text
+        Me.Close()
+    End Sub
+
+    Private Sub Cancel_Button_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Cancel_Button.Click
+        Me.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.Close()
+    End Sub
+
+    Private Sub txtInitials_TextChanged(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles txtInitials.TextChanged
+        If Me.txtInitials.Text.Length > 0 Then
+            Me.OK_Button.Enabled = True
+        Else
+            Me.OK_Button.Enabled = False
+        End If
+    End Sub
+
+End Class
