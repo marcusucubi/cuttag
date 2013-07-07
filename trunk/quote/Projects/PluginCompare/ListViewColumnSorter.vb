6f8e54d0358c0199c73418ee63ed28b8f5409f3b
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-07-07 19:06:43 +0000

    move 'find similar' feature to PluginCompare

---
+++ trunk/quote/Projects/PluginCompare/ListViewColumnSorter.vb
@@ -0,0 +1,53 @@
+﻿Public Class ListViewColumnSorter
+    Implements IComparer
+
+    Public Property SortColumn As Integer
+    Public Property Order As SortOrder
+    Public Property ObjectCompare As CaseInsensitiveComparer
+
+    Public Sub New()
+        SortColumn = 0
+        Order = SortOrder.None
+        ObjectCompare = New CaseInsensitiveComparer()
+    End Sub
+
+    Public Function Compare(ByVal x, ByVal y) As Integer _
+        Implements IComparer.Compare
+
+        Dim listviewX As ListViewItem = x
+        Dim listviewY As ListViewItem = y
+
+        Dim s1 As String = listviewX.SubItems(SortColumn).Text
+        Dim s2 As String = listviewY.SubItems(SortColumn).Text
+
+        If IsInteger(s1) And IsInteger(s2) Then
+            Dim i1 As Decimal = CDbl(s1)
+            Dim i2 As Decimal = CDbl(s2)
+            Dim compareResult As Integer
+            compareResult = (i1.CompareTo(i2))
+
+            If Order = SortOrder.Ascending Then
+                Return compareResult
+            ElseIf (Order = SortOrder.Descending) Then
+                Return (-compareResult)
+            End If
+        Else
+            Dim compareResult As Integer
+            compareResult = (s1.CompareTo(s2))
+
+            If Order = SortOrder.Ascending Then
+                Return compareResult
+            ElseIf (Order = SortOrder.Descending) Then
+                Return (-compareResult)
+            End If
+        End If
+
+        Return 0
+
+    End Function
+
+    Private Function IsInteger(ByVal s As String) As Boolean
+        Return IsNumeric(s)
+    End Function
+
+End Class
