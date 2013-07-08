6d53bc5eee5c773213bfa24a7ca81c57d2fb7ca4
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-07-08 23:52:38 +0000

    add filter button

--- quote/Projects/PluginPropertyView/frmComputationProperties.Designer.vb
+++ quote/Projects/PluginPropertyView/frmComputationProperties.Designer.vb
@@ -22,8 +22,54 @@ Partial Class frmComputationProperties
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.PropertyGrid1 = New CustomPropertyGrid()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(frmComputationProperties))
+        Me.ToolStripContainer1 = New System.Windows.Forms.ToolStripContainer()
+        Me.ToolStrip1 = New System.Windows.Forms.ToolStrip()
+        Me.FilterButton = New System.Windows.Forms.ToolStripButton()
+        Me.PropertyGrid1 = New PluginPropertyView.CustomPropertyGrid()
+        Me.ToolStripContainer1.ContentPanel.SuspendLayout()
+        Me.ToolStripContainer1.TopToolStripPanel.SuspendLayout()
+        Me.ToolStripContainer1.SuspendLayout()
+        Me.ToolStrip1.SuspendLayout()
         Me.SuspendLayout()
+        '
+        'ToolStripContainer1
+        '
+        '
+        'ToolStripContainer1.ContentPanel
+        '
+        Me.ToolStripContainer1.ContentPanel.Controls.Add(Me.PropertyGrid1)
+        Me.ToolStripContainer1.ContentPanel.Size = New System.Drawing.Size(284, 156)
+        Me.ToolStripContainer1.Dock = System.Windows.Forms.DockStyle.Fill
+        Me.ToolStripContainer1.Location = New System.Drawing.Point(0, 0)
+        Me.ToolStripContainer1.Name = "ToolStripContainer1"
+        Me.ToolStripContainer1.Size = New System.Drawing.Size(284, 181)
+        Me.ToolStripContainer1.TabIndex = 1
+        Me.ToolStripContainer1.Text = "ToolStripContainer1"
+        '
+        'ToolStripContainer1.TopToolStripPanel
+        '
+        Me.ToolStripContainer1.TopToolStripPanel.Controls.Add(Me.ToolStrip1)
+        '
+        'ToolStrip1
+        '
+        Me.ToolStrip1.Dock = System.Windows.Forms.DockStyle.None
+        Me.ToolStrip1.GripStyle = System.Windows.Forms.ToolStripGripStyle.Hidden
+        Me.ToolStrip1.Items.AddRange(New System.Windows.Forms.ToolStripItem() {Me.FilterButton})
+        Me.ToolStrip1.Location = New System.Drawing.Point(3, 0)
+        Me.ToolStrip1.Name = "ToolStrip1"
+        Me.ToolStrip1.Size = New System.Drawing.Size(71, 25)
+        Me.ToolStrip1.TabIndex = 0
+        '
+        'FilterButton
+        '
+        Me.FilterButton.CheckOnClick = True
+        Me.FilterButton.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text
+        Me.FilterButton.Image = CType(resources.GetObject("FilterButton.Image"), System.Drawing.Image)
+        Me.FilterButton.ImageTransparentColor = System.Drawing.Color.Magenta
+        Me.FilterButton.Name = "FilterButton"
+        Me.FilterButton.Size = New System.Drawing.Size(37, 22)
+        Me.FilterButton.Text = "Filter"
         '
         'PropertyGrid1
         '
@@ -31,25 +77,35 @@ Partial Class frmComputationProperties
         Me.PropertyGrid1.Location = New System.Drawing.Point(0, 0)
         Me.PropertyGrid1.Name = "PropertyGrid1"
         Me.PropertyGrid1.PropertySort = System.Windows.Forms.PropertySort.Categorized
-        Me.PropertyGrid1.Size = New System.Drawing.Size(284, 112)
-        Me.PropertyGrid1.TabIndex = 0
+        Me.PropertyGrid1.Size = New System.Drawing.Size(284, 156)
+        Me.PropertyGrid1.TabIndex = 1
         Me.PropertyGrid1.ToolbarVisible = False
         '
         'frmComputationProperties
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-        Me.ClientSize = New System.Drawing.Size(284, 112)
-        Me.Controls.Add(Me.PropertyGrid1)
+        Me.ClientSize = New System.Drawing.Size(284, 181)
+        Me.Controls.Add(Me.ToolStripContainer1)
         Me.DockAreas = CType(((((WeifenLuo.WinFormsUI.Docking.DockAreas.Float Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockLeft) _
-                    Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight) _
-                    Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) _
-                    Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom), WeifenLuo.WinFormsUI.Docking.DockAreas)
+            Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockRight) _
+            Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockTop) _
+            Or WeifenLuo.WinFormsUI.Docking.DockAreas.DockBottom), WeifenLuo.WinFormsUI.Docking.DockAreas)
         Me.Font = New System.Drawing.Font("Microsoft Sans Serif", 8.25!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
         Me.Name = "frmComputationProperties"
         Me.Text = "Costs"
+        Me.ToolStripContainer1.ContentPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.TopToolStripPanel.ResumeLayout(False)
+        Me.ToolStripContainer1.TopToolStripPanel.PerformLayout()
+        Me.ToolStripContainer1.ResumeLayout(False)
+        Me.ToolStripContainer1.PerformLayout()
+        Me.ToolStrip1.ResumeLayout(False)
+        Me.ToolStrip1.PerformLayout()
         Me.ResumeLayout(False)
 
     End Sub
-    Friend WithEvents PropertyGrid1 As CustomPropertyGrid
+    Friend WithEvents ToolStripContainer1 As System.Windows.Forms.ToolStripContainer
+    Friend WithEvents PropertyGrid1 As PluginPropertyView.CustomPropertyGrid
+    Friend WithEvents ToolStrip1 As System.Windows.Forms.ToolStrip
+    Friend WithEvents FilterButton As System.Windows.Forms.ToolStripButton
 End Class
