5234c171cffc4188a07faff0573a60df62de25b7
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-07-09 19:01:41 +0000

    cleanup

--- trunk/quote/Projects/PluginModel/common/DetailCollection.vb
+++ trunk/quote/Projects/PluginModel/common/DetailCollection.vb
@@ -13,20 +13,15 @@ Namespace Common
             _Header = pHeader
         End Sub
 
-        'dd_Added 10/8/11
         Private _IsSorted As Boolean = False
         Private _SortDirection As ListSortDirection = ListSortDirection.Ascending
         Private _SortProperty As PropertyDescriptor = Nothing
 
         Protected Overrides Function AddNewCore() As Object
-            Dim p As New Product("", 0, "", False, Nothing, Nothing) '("100", 2, "10", 1, True, "test", 0, "", 0, 0)
-            'Dim p As New Product("", 1, "10", False, Nothing, Nothing)
-            Dim o As Object = New Model.Template.Detail(_Header, p) 'ddAdded
-            ' Return New Quote.BOM.Detail(_Header, p) 'ddRemmed
-            Add(o) 'ddAdded
-            Return o 'ddAdded
-
-            'Return MyBase.AddNewCore()
+            Dim p As New Product("", 0, "", False, Nothing, Nothing)
+            Dim o As Object = New Model.Template.Detail(_Header, p)
+            Add(o)
+            Return o
         End Function
 
         Protected Overrides Sub OnAddingNew(ByVal e As System.ComponentModel.AddingNewEventArgs)
@@ -36,8 +31,6 @@ Namespace Common
         Public Overrides Sub CancelNew(ByVal itemIndex As Integer)
             MyBase.CancelNew(itemIndex)
         End Sub
-
-
 
         Protected Overrides Function FindCore(ByVal prop As System.ComponentModel.PropertyDescriptor, ByVal key As Object) As Integer
             Return MyBase.FindCore(prop, key)
@@ -52,32 +45,38 @@ Namespace Common
                 Return True
             End Get
         End Property
+
         Protected Overrides ReadOnly Property SupportsSortingCore As Boolean
             Get
                 Return True
             End Get
         End Property
+
         Protected Overrides ReadOnly Property SortDirectionCore As System.ComponentModel.ListSortDirection
             Get
                 Return Me._SortDirection
             End Get
         End Property
+
         Protected Overrides ReadOnly Property SortPropertyCore As System.ComponentModel.PropertyDescriptor
             Get
                 Return Me._SortProperty
             End Get
         End Property
+
         Protected Overrides ReadOnly Property IsSortedCore As Boolean
             Get
                 Return Me._IsSorted
             End Get
         End Property
+
         Protected Overrides Sub ApplySortCore(ByVal prop As System.ComponentModel.PropertyDescriptor, ByVal direction As System.ComponentModel.ListSortDirection)
             Me._SortDirection = direction
             Me._SortProperty = prop
             Dim oComparer As New DetailSortComparer(prop, direction)
             ApplySort2Detail(oComparer)
         End Sub
+
         Private Sub ApplySort2Detail(ByVal oComparer As DetailSortComparer)
             Dim listRef As List(Of Detail) = MyBase.Items
             If Not IsNothing(listRef) Then
@@ -86,7 +85,6 @@ Namespace Common
                 OnListChanged(New ListChangedEventArgs(ListChangedType.Reset, -1))
             End If
         End Sub
-        'dd_Added end
 
     End Class
 
