4e8787ad33de3e9f9841649feb7452847454dded
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-07-13 21:59:32 +0000

    update sample project

---
+++ trunk/quote/Projects/SampleProperties/frmTree.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace SampleProperties
+{
+    partial class frmTree
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // frmTree
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(226, 208);
+            this.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Name = "frmTree";
+            this.Text = "Calc Tree";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
---
+++ trunk/quote/Projects/SampleProperties/frmTree.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+
+using WeifenLuo.WinFormsUI.Docking;
+
+namespace SampleProperties
+{
+    public partial class frmTree : DockContent
+    {
+        public frmTree()
+        {
+            InitializeComponent();
+        }
+    }
+}
