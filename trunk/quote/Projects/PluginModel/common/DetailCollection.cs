201116b35e6e9050b6f7bfc7904d47fa1ed6eb3d
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-08-14 13:51:23 +0000

    minor changes to SavableProperties

--- trunk/quote/Projects/PluginModel/common/DetailCollection.cs
+++ trunk/quote/Projects/PluginModel/common/DetailCollection.cs
@@ -6,9 +6,12 @@ namespace Model.Common
     using System.Linq;
 
     public class DetailCollection<T> : 
-        System.Collections.ObjectModel.ObservableCollection<T> 
+        System.Collections.ObjectModel.ObservableCollection<T>,
+        ISavableProperties
         where T : Model.Common.Detail
     {
+        private bool dirty;
+        
         private Common.Header header;
         
         public DetailCollection(Common.Header header)
@@ -16,9 +19,68 @@ namespace Model.Common
             this.header = header;
         }
 
+        public event EventHandler Dirty;
+
+        public event EventHandler Clean;
+        
+        public bool IsDirty 
+        { 
+            get 
+            { 
+                return this.dirty; 
+            }
+            
+            set
+            {
+                if (value == true)
+                {
+                    this.dirty = true;
+                    
+                    if (this.Dirty != null) 
+                    {
+                        this.Dirty(this, new EventArgs());
+                    }
+                }
+                else
+                {
+                    this.dirty = false;
+                    
+                    foreach (ISavableProperties detail in this)
+                    {
+                        detail.IsDirty = false;
+                    }
+                    
+                    if (this.Clean != null) 
+                    {
+                        this.Clean(this, new EventArgs());
+                    }
+                }
+            }
+        }
+        
         public Common.Header Header
         {
             get { return this.header; }
+        }
+        
+        protected override void InsertItem(int index, T item)
+        {
+            base.InsertItem(index, item);
+            item.Dirty += this.OnDependentDirty;
+            this.IsDirty = true;
+        }
+        
+        protected override void RemoveItem(int index)
+        {
+            ISavableProperties item = this.Items[index];
+            base.RemoveItem(index);
+            item.Dirty -= this.OnDependentDirty;
+            this.IsDirty = true;
+        }
+        
+        private void OnDependentDirty(object source, EventArgs args)
+        {
+            this.IsDirty = true;
         }
     }
 }
---
+++ trunk/quote/Projects/PluginModelUnitTests/template/DirtyTest.cs
@@ -0,0 +1,38 @@
+﻿namespace PluginModelUnitTests.Template
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class DirtyTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Host.App.RegisteredClasses.Clear();
+                
+            Model.Template.Header header = new Model.Template.Header();
+            Model.Product product = new Model.Product();
+            Model.Template.Detail detail = new Model.Template.Detail(header, product);
+            header.Details.Add(detail);
+            
+            header.IsDirty = false;
+            Assert.IsFalse(header.IsDirty);
+            
+            Model.Template.NoteProperties notes = header.NoteProperties as Model.Template.NoteProperties;
+            Assert.IsFalse(notes.IsDirty);
+            notes.Note = "test";
+            Assert.IsTrue(notes.IsDirty);
+            Assert.IsTrue(header.IsDirty);
+            
+            header.IsDirty = false;
+            Assert.IsFalse(notes.IsDirty);
+            
+            detail.Qty = 2;
+            Assert.IsTrue(header.IsDirty);
+            header.IsDirty = false;
+            Assert.IsFalse(detail.IsDirty);
+            
+        }
+    }
+}
