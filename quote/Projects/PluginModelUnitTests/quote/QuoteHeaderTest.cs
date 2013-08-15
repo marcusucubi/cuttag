018760f6fe49bdf356b2bf99c513fa1554bd791b
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-08-15 21:49:24 +0000

    add unit tests for model

---
+++ quote/Projects/PluginModelUnitTests/quote/QuoteHeaderTest.cs
@@ -0,0 +1,30 @@
+﻿namespace PluginModelUnitTests.Quote
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class QuoteHeaderTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            DateTime now = DateTime.Now;
+            
+            Model.Quote.Header header = new Model.Quote.Header(
+                1, "test", "test", "test", now, now, 2);
+            
+            Assert.That(header.IsQuote, Is.True);
+            Assert.That(header.DisplayName, Is.EqualTo("Quote 1"));
+            Model.Common.Detail detail = header.NewDetail(new Model.Product());
+            Assert.That(detail, Is.Not.Null);
+            
+            header.SetPublicPrimaryProperties(new Model.Common.PrimaryProperties());
+            header.SetPublicComputationProperties(new Model.Common.ComputationProperties());
+            header.SetPublicOtherProperties(new Model.Common.OtherProperties());
+            header.SetPublicNoteProperties(new Model.Common.NoteProperties());
+            
+            Model.Quote.Header header2 = new Model.Quote.Header();
+        }
+    }
+}
