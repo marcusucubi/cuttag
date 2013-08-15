ebf322eb112cb90767f775191c5b8c80d7a7b8d4
marcusucubi@gmail.com <marcusucubi@gmail.com@e5946206-da35-2a59-e6c2-62807b22cc5c>
2013-08-15 21:49:24 +0000

    add unit tests for model

---
+++ trunk/quote/Projects/PluginModelUnitTests/quote/QuoteDetailTest.cs
@@ -0,0 +1,27 @@
+﻿namespace PluginModelUnitTests.Quote
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class QuoteDetailTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            Model.Quote.Header header = new Model.Quote.Header();
+            
+            Model.ProductBuildData data = new Model.ProductBuildData() { IsWire=true };
+            Model.Product product = new Model.Product(data);
+            
+            Model.Quote.Detail detail = (Model.Quote.Detail)header.NewDetail(product);
+            
+            Model.Quote.QuoteSavableProperties props = new Model.Quote.QuoteSavableProperties();
+            detail.SetProperties(props);
+            Assert.That(detail.QuoteDetailProperties, Is.EqualTo(props));
+            
+            detail.Qty = 10;
+            Assert.That(detail.Qty, Is.EqualTo(10));
+        }
+    }
+}
---
+++ trunk/quote/Projects/PluginModelUnitTests/quote/QuotePrimaryPropertiesTest.cs
@@ -0,0 +1,29 @@
+﻿namespace PluginModelUnitTests.Quote
+{
+    using System;
+    using NUnit.Framework;
+
+    [TestFixture]
+    public class QuotePrimaryPropertiesTest
+    {
+        [Test]
+        public void TestMethod()
+        {
+            DateTime now = DateTime.Now;
+            
+            Model.Quote.PrimaryProperties props = new Model.Quote.PrimaryProperties(
+                1, "test", "test", "test", now, now, 2);
+            
+            Assert.That(props.CreatedDate, Is.EqualTo(now));
+            Assert.That(props.LastModified, Is.EqualTo(now));
+            Assert.That(props.QuoteNumber, Is.EqualTo(1));
+            Assert.That(props.Initials, Is.EqualTo("test"));
+            Assert.That(props.Customer, Is.Null);
+            Assert.That(props.PartNumber, Is.EqualTo("test"));
+            Assert.That(props.RequestForQuoteNumber, Is.EqualTo("test"));
+            Assert.That(props.TemplateNumber, Is.EqualTo(2));
+                
+            props.SetTemplateId(10);
+        }
+    }
+}
